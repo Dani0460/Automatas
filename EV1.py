13346c34659ddfb084b2fe827d540bb1bd068d7f
Dani0460 <47428621+Dani0460@users.noreply.github.com>
2021-02-27 04:47:14 +0000

    Test de EV1 teoría de autom

---
+++ EV1.py
@@ -0,0 +1,12 @@
+import exrex
+
+lista=(list(exrex.generate('\d((\.|E)((a|b){1}|E)){1}(E|\.)dacp((((\.|E)(a|b){1}){1})|E)\.1795240')))
+#matching = [s for s in lista if "dacp" in s]
+
+print(lista)
+#(d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0)
+#((\.|E)(a|b){1}){2}
+
+
+
+
