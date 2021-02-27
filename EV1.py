7e83a1d14c9e077da5d09a63045d9e02a3c97221
Dani0460 <47428621+Dani0460@users.noreply.github.com>
2021-02-27 01:57:03 -0600

    Mejora en muestreo de datos
    Se recorre la lista en busca de las cadenas con E(epsilon) y se elimina ese simbolo.

--- EV1.py
+++ EV1.py
@@ -1,11 +1,14 @@
 import exrex
 
 lista=(list(exrex.generate('\d{1}((\.|E)((a|b){1}|E)){1}(E|\.)dacp((((\.|E)(a|b){1}){1})|E)\.1795240')))
-#matching = [s for s in lista if "dacp" in s]
+for i in range(len(lista)):
+    
+    if('E' in lista[i]):
+        lista[i]=lista[i].replace('E','')
 
-print(lista)
+    print(lista[i])
 #(d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0)
-#((\.|E)(a|b){1}){2}
+#((\.|E)(a|b){1}){1}
 
 
 
