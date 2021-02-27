20a6086c9a62fe0655949ba0a21b7b694b7f16ba
Dani0460 <47428621+Dani0460@users.noreply.github.com>
2021-02-27 02:45:17 -0600

    La lista se convierte a diccionario
    La lista se convirtió en diccionario porque me percaté que al imprimir la lista habían muchos elementos repetidos.

--- EV1.py
+++ EV1.py
@@ -1,14 +1,23 @@
 import exrex
 
-lista=(list(exrex.generate("(1|7|9|5|2|4|0){1}((\.|E)((a|b){1}|E)){1}(E|\.)dacp((((\.|E)(a|b){1}){1})|E)\.1795240")))
+lista=(list(exrex.generate("(1|7|9|5|2|4|0){1}((\.|E)(d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0){1}){1}(\.|E){1}dacp((((\.|E)(d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0){1}){1})|E)\.1795240")))
+
+print("Sanitizando...")
 for i in range(len(lista)):
-    
     if('E' in lista[i]):
         lista[i]=lista[i].replace('E','')
 
-    print(lista[i])
+print("Ya casi terminamos xddd")
+lista = list(dict.fromkeys(lista))
+print(lista)
+
+
+
 #(d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0)
 #((\.|E)(a|b){1}){1}
+"""print("S={d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0|.}")
+print("ingresa una cadena de texto, toma en cuenta el alfabeto")
+s=input()"""
 
 
 
