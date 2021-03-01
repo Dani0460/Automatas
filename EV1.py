f92d435885c081bfcfe48ae98c3c25af862b8e62
Dani0460 <47428621+Dani0460@users.noreply.github.com>
2021-02-28 19:43:42 -0600

    versión final del proyecto
    se eliminó todo lo anterior ya que solo era cuestión de verificar el string con el regex, no hacía falta guardar todas las posibilidades.

--- EV1.py
+++ EV1.py
@@ -1,24 +1,12 @@
-import exrex
-
-lista=(list(exrex.generate("(1|7|9|5|2|4|0){1}((\.|E)(d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0){1}){1}(\.|E){1}dacp((((\.|E)(d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0){1}){1})|E)\.1795240")))
-
-print("Sanitizando...")
-for i in range(len(lista)):
-    if('E' in lista[i]):
-        lista[i]=lista[i].replace('E','')
-
-print("Ya casi terminamos xddd")
-lista = list(dict.fromkeys(lista))
-print(lista)
-
-
-
-#(d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0)
-#((\.|E)(a|b){1}){1}
-"""print("S={d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0|.}")
+import re
+print("S={d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0|.}")
 print("ingresa una cadena de texto, toma en cuenta el alfabeto")
-s=input()"""
-
+txt=input()
+match=re.match("^((1|7|9|5|2|4|0)+)((\.|E)((d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0)+))*(E|\.)(dacp)(((\.|E)(d|a|n|i|e|l|j|r|o|c|u|z|1|7|9|5|2|4|0)+)*|E)(\.1795240$)",txt)
+txt=txt.replace('E','')
+print(txt)
+is_match=bool(match)
+print(is_match)
 
 
 
